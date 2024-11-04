fa86615fabae5b5065d58416da04c9991c660879
Practice <coding4443@gmail.com>
2024-11-04 10:20:57 -0800

    changes in db connection

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -6,8 +6,6 @@ dotenv.config();
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
         });
         console.log('MongoDB connected successfully');
     } catch (error) {
